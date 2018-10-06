f1e2f0a9521646bc1b3f04deda1ba224d15cc5f9
vinaydhankhar <vinaydhankhar6@gmail.com>
2018-10-06 12:25:21 +0530

    from vinay

--- app/src/main/java/kvnb/hostelservicemanagement/MainActivity.java
+++ app/src/main/java/kvnb/hostelservicemanagement/MainActivity.java
@@ -1,13 +1,29 @@
 package kvnb.hostelservicemanagement;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button b1;
+    public void init(){
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent=new Intent(MainActivity.this,Login.class);
+                startActivity(myIntent);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
 }
